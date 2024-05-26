b40d2d8b5c9cce97c0908fd8844fa5c6d1fb9845
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-26 13:06:35 -0500

    creacion del proyecto y agregacion de dependencias para los graficos

---
+++ src/main/java/org/jhonatan/app/GraficoCircular.java
@@ -0,0 +1,12 @@
+package org.jhonatan.app;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class GraficoCircular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
